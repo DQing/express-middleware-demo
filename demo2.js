e8087fe56da2fda7d8040b6a359f59d1bb0cf235
DQing <15829258737@163.com>
2017-06-06 12:35:54 +0800

    add next() route skip function

--- demo2.js
+++ demo2.js
@@ -1,3 +1,28 @@
-/**
- * Created by douqing on 17-6-6.
- */
+/*
+* next()函数的路由跳转
+* */
+var express = require('express');
+var app = express();
+app.use(function (req, res, next) {
+    console.log('app use1');
+    next();
+});
+
+app.get('/:name', function (req, res, next) {
+    res.send('hello' + req.params.name);
+    next('route');
+}, function (req, res) {
+    console.log('middle');
+});
+
+app.get('/:name', function (req, res) {
+    console.log('my name is douqing');
+});
+
+app.use(function (req, res) {
+    console.log('app use2');
+});
+
+app.listen(3000, function () {
+    console.log('listen 3000 port');
+});
