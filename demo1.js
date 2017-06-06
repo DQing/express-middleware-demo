3169cb425b08a7fbcb63c56265c3f707087148a2
DQing <15829258737@163.com>
2017-06-06 12:23:09 +0800

    add next first function

--- demo1.js
+++ demo1.js
@@ -1,10 +1,31 @@
+/*
+* next()函数中间件传递
+* */
+
 var express = require('express');
 var app = express();
 
-app.get('/',function (req, res) {
+app.use(function (req, res, next) {
+    console.log('app use2');
+    next();
+
+});
+app.get('/', function (req, res, next) {
     res.send('hello');
+    next();
+}, function (req, res, next) {
+    console.log('world');
+    next();
 });
 
-app.listen(3000,function () {
+app.get('/', function (req, res) {
+    // res.send('second');
+    console.log('two');
+});
+
+app.use(function (req, res) {
+    console.log('app use2');
+});
+app.listen(3000, function () {
     console.log('listen 3000 port');
 });
