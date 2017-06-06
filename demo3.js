eafd4c29ac27a5d48dce0be742c768c2a6c46135
DQing <15829258737@163.com>
2017-06-06 13:00:27 +0800

    add next error function

--- demo3.js
+++ demo3.js
@@ -1,3 +1,24 @@
-/**
- * Created by douqing on 17-6-6.
- */
+var express = require('express');
+var app = express();
+
+//
+// app.use(function (err, req, res, next) {
+//     res.send('ss');
+//     console.log("we have an error: " + err);
+// });
+
+// a middleware bind to a path
+app.get('/', function (req, res, next) {
+    res.send('hello');
+    next('my-error');
+}, function (req, res) {
+    console.log('test');
+});
+
+app.get('/', function (req, res) {
+    console.log('finish');
+});
+
+app.listen(3000, function () {
+    console.log('listen 3000 port');
+});
